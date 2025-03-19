a0c3b17d4bfb6cd48a9b4099f7757c1257a7eea9
Nadufa <nadufa18404@gmail.com>
2025-03-19 13:36:37 +0300

    refactor: withQueryStatus hoc

--- src/shared/hocs/withQueryStatus/withQueryStatus.tsx
+++ src/shared/hocs/withQueryStatus/withQueryStatus.tsx
@@ -3,45 +3,45 @@ import { ComponentType } from "react";
 import { ErrorBlock } from "../../ui";
 import { IWithQueryStatus } from "./types";
 
-export const withQueryStatus = (() => {
-  return <T extends IWithQueryStatus>(Component: ComponentType<T>) => {
-    return (props: T) => {
-      const {
-        isLoading,
-        isError,
-        isEmptyData,
-        emptyDataLabel,
-        errorBlockChildren,
-      } = props;
-      if (isLoading) {
-        return (
-          <Box pos={"relative"} flex={1}>
-            <LoadingOverlay
-              visible
-              overlayProps={{ blur: 1 }}
-              loaderProps={{ color: "#7c609a", size: 100 }}
-            />
-          </Box>
-        );
-      }
+export const withQueryStatus = <T extends IWithQueryStatus>(
+  Component: ComponentType<T>
+) => {
+  return (props: T) => {
+    const {
+      isLoading,
+      isError,
+      isEmptyData,
+      emptyDataLabel,
+      errorBlockChildren,
+    } = props;
+    if (isLoading) {
+      return (
+        <Box pos={"relative"} flex={1}>
+          <LoadingOverlay
+            visible
+            overlayProps={{ blur: 1 }}
+            loaderProps={{ color: "#7c609a", size: 100 }}
+          />
+        </Box>
+      );
+    }
 
-      if (isError) {
-        return errorBlockChildren ? (
-          <ErrorBlock>{errorBlockChildren}</ErrorBlock>
-        ) : (
-          <ErrorBlock />
-        );
-      }
+    if (isError) {
+      return errorBlockChildren ? (
+        <ErrorBlock>{errorBlockChildren}</ErrorBlock>
+      ) : (
+        <ErrorBlock />
+      );
+    }
 
-      if (isEmptyData) {
-        return (
-          <Center h={"100%"} fz={"h3"} fw={"bold"}>
-            {emptyDataLabel ?? "No data"}
-          </Center>
-        );
-      }
+    if (isEmptyData) {
+      return (
+        <Center h={"100%"} fz={"h3"} fw={"bold"}>
+          {emptyDataLabel ?? "No data"}
+        </Center>
+      );
+    }
 
-      return <Component {...props} />;
-    };
+    return <Component {...props} />;
   };
-})();
+};
